c41f61f8c25366aace154d5e9383fc028ca94468
zadik <zadikmccold@gmail.com>
2014-10-24 11:52:21 +0200

    new class

---
+++ empresa.py
@@ -0,0 +1,11 @@
+__author__ = 'zadik'
+
+class empresa:
+    def __init__(self,nombre_empresa,cif,razon_social):
+        self.nombre_empresa=nombre_empresa
+        self.cif=cif
+        self.razon_social=razon_social
+        self.lista_depto=[]
+
+    def
+
